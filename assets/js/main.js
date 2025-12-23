2336a98b90e7dd15e63af95dd573862403fe0125
veankatakrishnan <veankatakrishnan@gmail.com>
2025-12-23 12:16:15 +0530

    Changed the color theme of the whole website and added light and dark switching and Added projects section and removed the Read me from the template

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ const typed = new Typed('#home-typed', {
     backSpeed: 40,
     backDelay: 2000,
     loop: true,
-    cursorChar: '',
+    cursorChar: '|',
 });
 
 /*=============== ADD SHADOW HEADER ===============*/
@@ -100,3 +100,110 @@ const sendEmail = (e) => {
 };
 
 contactForm.addEventListener('submit', sendEmail);
+
+
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '10px',
+    duration: 400,
+    reset: false,
+    opacity: 0,
+    scale: 1,
+    easing: 'ease-in-out',
+    viewFactor: 0.2
+})
+
+sr.reveal('.section__title')
+sr.reveal('.section__subtitle', {delay: 200})
+sr.reveal('.projects__subtitle', {delay: 300})
+sr.reveal('.home__content, .about__container, .education__heading, .timeline__component, .skills__scroller, .projects__container, .contact__container', {delay: 400})
+sr.reveal('.home__social, .about__skills, .timeline__skills, .contact__social', {delay: 500, origin: 'bottom'})
+sr.reveal('.home__image, .about__image, .timeline__image-wrapper', {delay: 600, origin: 'bottom'})
+sr.reveal('.projects__card', {interval: 100})
+
+/*=============== SKILLS SCROLLER ===============*/
+const scrollers = document.querySelectorAll(".skills__scroller");
+
+// If a user hasn't opted in for reduced motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
